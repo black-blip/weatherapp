877c83258308a043c040d2271224056459d7e5d6
black-blip <sakshipatil575@gmail.com>
2020-12-08 10:33:26 +0530

    final

--- weather/views.py
+++ weather/views.py
@@ -4,21 +4,22 @@ from django.views.generic import View
 from .models import City
 # Create your views here.
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={API key}'
     cities = City.objects.all()
 
     data = []
     city = 'Las Vegas'
-    for city in cities:
+    cityweather = {}
+    
 
-        r = requests.get(url.format(city)).json()
-        cityweather = {
-            'city':city,
-            'temperature':r['main']['temp'],
-            'description':r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon']
+    r = requests.get(url.format(city)).json()
+    cityweather = {
+        'city':city,
+        'temperature':r['main']['temp'],
+        'description':r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon']
         }
-        data.append(cityweather)
+    data.append(cityweather)
         
 
     context = {
