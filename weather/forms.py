6917ccf2867df606d97a115aed3c92fa31249d06
black-blip <sakshipatil575@gmail.com>
2020-12-11 19:55:20 +0530

    added new tables

--- weather/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import City
-from django.forms import ModelForm, TextInput
-
-class cityform(ModelForm):
-    class Meta:
-        model = City
-        fields = ['Name']
-        widgets = {'name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'})}
--- weather/views.py
+++ weather/views.py
@@ -1,30 +1,29 @@
-import requests
 from django.shortcuts import render
-from django.views.generic import View
+import requests
 from .models import City
-# Create your views here.
+
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={API key}'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=67202aa61e9bebd603b24c7514ea3098'
+    city = 'London'
     cities = City.objects.all()
 
-    data = []
-    city = 'Las Vegas'
-    cityweather = {}
-    
-
-    r = requests.get(url.format(city)).json()
-    cityweather = {
-        'city':city,
-        'temperature':r['main']['temp'],
-        'description':r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon']
+    weather_data = []
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city':city.name,
+            'temperature':r['main']['temp'],
+            'description':r['weather'][0]['description'],
+            'icon':r['weather'][0]['icon']
         }
-    data.append(cityweather)
-        
 
-    context = {
-        'cityweather': cityweather
-    }
-
+        weather_data.append(city_weather)
+    
+    context = {'weather_data':weather_data}
 
     return render(request,'weather/index.html',context)
+
+
+def search(request):
+
+    return render(request,'weather/index.html')
