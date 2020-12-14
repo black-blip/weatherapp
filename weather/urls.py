4dafac356322b598d12c6cbd88fa0fdba6f3121f
black-blip <sakshipatil575@gmail.com>
2020-12-14 20:15:18 +0530

    Finished Backend

--- weather/urls.py
+++ weather/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    
+    path('search/',views.search,name='search')    
 ]
+
--- weather/views.py
+++ weather/views.py
@@ -2,28 +2,29 @@ from django.shortcuts import render
 import requests
 from .models import City
 
+
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=67202aa61e9bebd603b24c7514ea3098'
-    city = 'London'
-    cities = City.objects.all()
-
-    weather_data = []
-    for city in cities:
-        r = requests.get(url.format(city)).json()
-        city_weather = {
-            'city':city.name,
-            'temperature':r['main']['temp'],
-            'description':r['weather'][0]['description'],
-            'icon':r['weather'][0]['icon']
-        }
-
-        weather_data.append(city_weather)
-    
-    context = {'weather_data':weather_data}
-
-    return render(request,'weather/index.html',context)
+    return render(request,'weather/index.html')
 
 
 def search(request):
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
+    context = {}
+    if 'search' in request.GET:
+        city = request.GET.get('search')
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city':city,
+            'temperature':r['main']['temp'],
+            'description':r['weather'][0]['description'],
+            'icon':r['weather'][0]['icon'],
+            'feels_like': r['main']['feels_like'],
+            'min_temp':r['main']['temp_min'],
+            'max_temp':r['main']['temp_max'],
+            'humidity':r['main']['humidity'],
 
-    return render(request,'weather/index.html')
+        }
+
+        context = {'city_weather':city_weather}
+
+    return render(request,'weather/index.html',context)
